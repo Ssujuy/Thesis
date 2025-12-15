63910e015266bcbfc1856a634049a0e589293d08
Ssujuy <kostas.arvanitis19@gmail.com>
2025-12-15 17:36:02 +0200

    Adding docstring for HexamerScore and FickettScore classes, fixing typos,bugs and format

--- computationalFeatures.py
+++ computationalFeatures.py
@@ -6,7 +6,41 @@ import math
 import Helpers
 
 class HexamerScore():
+    """
+    Class that initializes look-up tables from coding and non-coding small open reading frame DNA samples for Hexamer Score calculation.
+    Look-up tables are used to calculate Hexamer score score of any given DNA sequence using `score` function.
+    
+    Attributes
+    ----------
+
+    k : int
+        Number k of k-mer (split of DNA sequence).
+    
+    bases : list
+        List of valid bases inside a DNA sequence.
+    
+    codingHexamersFreq : dict
+        Dictionary for coding smORFs with bases as keys and frequency as value.
+
+    nonCodingHexamersFreq : dict
+        Dictionary for non-coding smORFs with bases as keys and frequency as value.
+
+    Methods
+    ----------
+    score(sequence: str) -> float
+        Calculates and returns Hexamer Score of a given DNA sequence. 
+    """
+
     def __init__(self, sequencesPath: str):
+        """
+        Constructs look-up tables that calculate hexamer frequencies, from known coding and non-coding smORF samples.
+
+        Parameters
+        ----------
+        sequencesPath : str
+            Path to csv file containing [sequence, label] columns of known coding and non-coding sequences.
+        """
+
         self.k = 6
         self.bases = ['A', 'C', 'G', 'T']
 
@@ -18,6 +52,10 @@ class HexamerScore():
         df = pd.read_csv(sequencesPath)
 
         for sequence, label in zip(df["sequence"], df["label"]):
+
+            if len(sequence) < self.k:
+                raise ValueError(f"Sequence given is less than {self.k} length")
+
             if 'N' not in sequence:
                 if label == 1:
                     kmerSequence = Helpers.kmer(sequence, self.k)
@@ -38,6 +76,23 @@ class HexamerScore():
             self.nonCodingHemaerFreq[key] = (value + 1.0) / totalNonCoding
 
     def score(self, sequence: str) -> float:
+        """
+        Calculates the Hexamer Score of a given DNA sequence, using initialized look-up tables.
+
+        Parameters
+        ----------
+        sequence : str
+            DNA sequence given to calculate its Hexamer Score.
+        
+        Return
+        ----------
+        float
+            Calculated Hexamer Score.
+        """
+
+        if len(sequence) < self.k:
+            raise ValueError(f"Sequence given is less than {self.k} length")
+
         total = 0.0
         p = 0.0
         kmerSequence = Helpers.kmer(sequence, self.k)
@@ -49,8 +104,46 @@ class HexamerScore():
         return p / total
     
 class FicketScore():
+    """
+    Class that initializes look-up tables from coding and non-coding small open reading frame DNA samples for Fickett Score calculation.
+    Look-up tables are initialized by calculating each base frequencies and periodicity bias by splitting the sequence into 3 frames.
+    Then the frequency of each base is calculated for each frame and the stats are appended to their designated coding potential (coding vs non-coding).
+    
+    Attributes
+    ----------  
+    bases : list
+        List of valid bases inside a DNA sequence.
+    
+    codingFickettStats : dict
+        Dictionary containing `mean` and `std` calculations of coding Fickett stats (base frequencies and periodicity biases).
+
+    nonCodingHexamersFreq : dict
+        Dictionary containing `mean` and `std` calculations of non-coding Fickett stats (base frequencies and periodicity biases).
+
+    Methods
+    ----------
+    _baseFrequencies(self, sequence: str) -> dict:
+        Calculates frequency for each base on a given sequence.
+    
+    _frameFrequencies(self, sequence: str) -> dict:    
+        Calculates frequency for each base on a frame of a sequence.
+    
+    _calculatePeriodicityBias(self, frequencies: dict) -> dict:
+        Calculates periodicity bias (maximum frequency / minimum frequency : for each base).
+    
+    score(sequence: str) -> float
+        Calculates and returns Fickett Score of a given DNA sequence.
+    """
 
     def __init__(self, sequencesPath: str):
+        """
+        Constructs look-up tables that calculate fickett stats, from known coding and non-coding smORF samples.
+
+        Parameters
+        ----------
+        sequencesPath : str
+            Path to csv file containing [sequence, label] columns of known coding and non-coding sequences.
+        """
 
         self.bases = ['A', 'C', 'G', 'T']
         self.codingFickettStats = {}
@@ -62,6 +155,9 @@ class FicketScore():
         df = pd.read_csv(sequencesPath)
 
         for sequence, label in zip(df["sequence"], df["label"]):
+
+            if len(sequence) < 3:
+                raise ValueError("Sequence given is less than 3 nucleotide length")
 
             baseFreq = self._baseFrequencies(sequence)
             framesFreq = self._frameFrequencies(sequence)
@@ -80,16 +176,30 @@ class FicketScore():
             else:
                 nonCodingFeatures.append(features)
 
-        coding_features_np = np.array(codingFeatures)
-        noncoding_features_np = np.array(nonCodingFeatures)
+        codingFeatures = np.array(codingFeatures)
+        nonCodingFeatures = np.array(nonCodingFeatures)
 
-        self.codingFickettStats['mean'] = coding_features_np.mean(axis=0)
-        self.codingFickettStats['std'] = coding_features_np.std(axis=0) + 1e-9 
+        self.codingFickettStats['mean'] = codingFeatures.mean(axis=0)
+        self.codingFickettStats['std'] = codingFeatures.std(axis=0) + 1e-9 
         
-        self.nonCodingFickettStats['mean'] = noncoding_features_np.mean(axis=0)
-        self.nonCodingFickettStats['std'] = noncoding_features_np.std(axis=0) + 1e-9
+        self.nonCodingFickettStats['mean'] = nonCodingFeatures.mean(axis=0)
+        self.nonCodingFickettStats['std'] = nonCodingFeatures.std(axis=0) + 1e-9
 
     def _baseFrequencies(self, sequence: str) -> dict:
+        """
+        Calculates frequency for each base on a given sequence.
+        Returns dictionary with bases as keys and frequencies as values.
+        
+        Parameters
+        ----------
+        sequence : str
+            DNA sequence as string.
+
+        Return
+        ----------
+        dict
+            Dictionary containing frequencies for all bases.
+        """
 
         frequencies = {}
         sequenceLength = len(sequence)
@@ -102,6 +212,19 @@ class FicketScore():
         return frequencies
 
     def _frameFrequencies(self, sequence: str) -> dict:
+        """
+        Calculates frequencies for all bases, on a seqeunce split into 3 frames.
+
+        Parameters
+        ----------
+        sequence : str
+            DNA sequence as string.
+
+        Return
+        ----------
+        dict
+            Dictionary containing frequencies for all bases for all 3 frames.
+        """
 
         frame1 = sequence[0::3]
         frame2 = sequence[1::3]
@@ -120,6 +243,19 @@ class FicketScore():
         return frequencies
     
     def _calculatePeriodicityBias(self, frequencies: dict) -> dict:
+        """
+        Calculates periodicity bias for all bases from the frequencies produced by the 3 frames.
+
+        Parameters
+        ----------
+        frequencies : dict
+            Contains frequences for each base and for each frame.
+
+        Return
+        ----------
+        dict
+            Dictionary containing periodicity bias for each base.
+        """
 
         bias = {}
 
@@ -142,11 +278,21 @@ class FicketScore():
         """
         Calculates the Fickett Score for a given sequence by comparing its\n
         raw features against the trained coding and non-coding distributions.\n
-        This method returns a single scalar score, that acts as a strong feature for the classifier.
+        This method returns a single scalar score.
+
+        Parameters
+        ----------
+        sequence : str
+            DNA sequence as string.
+
+        Return
+        ----------
+        float
+            Fickett Score for given DNA sequence.
         """
 
         if len(sequence) < 3:
-            raise ValueError("Sequence given is less than 3 nucleotide length")
+            raise ValueError("Sequence given is less than 3 length")
 
         baseFreq = self._baseFrequencies(sequence)
         framesFreq = self._frameFrequencies(sequence)
